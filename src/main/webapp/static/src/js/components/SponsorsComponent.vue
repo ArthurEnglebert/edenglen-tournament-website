05afa9c305a22a7c11d841e974e25ba987463d73
Tim Mercier <info@scrily.be>
2018-06-05 22:28:21 +0200

    Last changes + sponsors images

--- src/main/webapp/static/src/js/components/SponsorsComponent.vue
+++ src/main/webapp/static/src/js/components/SponsorsComponent.vue
@@ -25,11 +25,26 @@
                     arrows: false,
                     autoplay: true,
                     autoplaySpeed: 1000,
+                    adaptiveHeight: true,
                     speed: 1000
                 },
                 slides: [
+                    {img: '/static/dist/assets/img/sponsors/familyhouse.jpg', link: null},
+                    {img: '/static/dist/assets/img/sponsors/tstbat.jpg', link: null},
+                    {img: '/static/dist/assets/img/sponsors/consilium.png', link: null},
                     {img: '/static/dist/assets/img/sponsors/puilaetcodewaay.jpg', link: 'https://www.puilaetcodewaay.be/fr'},
-                    {img: '/static/dist/assets/img/sponsors/puilaetcodewaay.jpg', link: 'https://www.puilaetcodewaay.be/fr'},
+                    {img: '/static/dist/assets/img/sponsors/cossup.png', link: 'http://cossup.be/'},
+                    {img: '/static/dist/assets/img/sponsors/rodolphecruysmans.png', link: null},
+                    {img: '/static/dist/assets/img/sponsors/puttinggolfstudio.jpg', link: "http://www.puttinggolfstudio.com/"},
+                    {img: '/static/dist/assets/img/sponsors/bedimo.jpg', link: "http://www.bedimo.com/accueil/"},
+                    {img: '/static/dist/assets/img/sponsors/maxelassurance.jpg', link: "http://maxelassurance.be/"},
+                    {img: '/static/dist/assets/img/sponsors/pro1gold.png', link: "http://www.pro1golf.com/"},
+                    {img: '/static/dist/assets/img/sponsors/mrbricolage.png', link: "https://www.mr-bricolage.be/"},
+                    {img: '/static/dist/assets/img/sponsors/madeinlouise.gif', link: "https://www.madeinlouise.com/"},
+                    {img: '/static/dist/assets/img/sponsors/dbo.jpeg', link: null},
+                    {img: '/static/dist/assets/img/sponsors/dlcw.png', link: "http://www.dlcw.be"},
+                    {img: '/static/dist/assets/img/sponsors/natsrawline.png', link: "http://www.natsrawline.com/fr/"},
+                    {img: '/static/dist/assets/img/sponsors/scrily.png', link: "https://www.scrily.be"},
                 ]
             };
         },
